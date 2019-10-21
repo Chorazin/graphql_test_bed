586afd48cb9ce6edaf61af12082c417c15e3c367
Roger Atkins <chorazin@gmail.com>
2019-10-21 16:25:45 +1100

    rm

--- server/database/init_db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.init_mongo_db = () => {
-  mongoose.connect('mongodb+srv://Chorazin:Explicit6669@graphql-test-bed-sfjav.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-  mongoose.connection.once('open', () => {
-    console.log('connected to database');
-  });
-}
